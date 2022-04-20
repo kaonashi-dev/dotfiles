843f7c691ae2445220a0020324c962fe225c1e7c
samueldlh <samueldlh.dev@gmail.com>
2022-04-19 22:42:49 -0500

    polybar bar config

---
+++ config/polybar/memory.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mem=`free | awk '/Mem/ {printf "%dM/%dM\n", $3 / 1024.0, $2 / 1024.0 }'`
+echo -e "$mem"
--- config/spectrwm/autostart.sh
+++ config/spectrwm/autostart.sh
@@ -2,9 +2,9 @@
 
 screens=`xrandr | grep ' connected' | grep 'HDMI' | awk '{print $1}'`
 
-if [ "$screens" = "HDMI-1" ]; 
+if [ "$screens" = "HDMI1" ]; 
 then
-   xrandr --output VGA-1 --mode 1366x768 --pos 1920x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off
+   xrandr --output VGA1 --mode 1366x768 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off
 fi
 
 setxkbmap us -variant intl

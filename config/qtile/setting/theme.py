0e8ed780bda1496963ad1d06a8a8f2d40936b881
Samuel <samueldlh.dev@gmail.com>
2023-03-19 16:08:53 -0500

    remove qtile config

--- config/qtile/setting/theme.py
+++
@@ -1,9 +0,0 @@
-colors = {
-   'color1': '#000000',
-   'color2': '#F283B6',
-   'color3': '#E4B7E5',
-   'color4': '#EAF0CE',
-   'color5': '#EDBFB7',
-   'color6': '#918EF4',
-   'color7': '#FFFFFF',
-}
--- config/spectrwm/autostart.sh
+++ config/spectrwm/autostart.sh
@@ -2,19 +2,19 @@
 
 screens=`xrandr | grep ' connected' | grep 'HDMI' | awk '{print $1}'`
 
-if [ "$screens" = "HDMI1" ]; 
+if [ "$screens" = "HDMI-1" ]; 
 then
-   xrandr --output VGA1 --mode 1366x768 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off
+   xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
 fi
 
 setxkbmap us -variant intl
 
-# volumeicon &
+volumeicon &
 
-# cbatticon &
+cbatticon &
 
 picom &
 
-nm-tray &
+dunst &
 
 nitrogen --restore
